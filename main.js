76c02d998418e36c51a937f5f5cd12e073339dcc
Alexander Galushka <alexander.galushka@verizonmedia.com>
2022-04-12 08:52:19 -0700

    fix: missing async

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
 
-function handleRequest(_req) {
+async function handleRequest(_req) {
   // Respond with HTML
 
   const html = await Deno.readFile("./index.html");
